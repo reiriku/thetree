eb26553fdb01e23829701ad7e0b5577c1da8a427
REIRIKU <cookiecat3003@gmail.com>
2025-10-06 13:47:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1349,7 +1349,7 @@ app.use((err, req, res, _) => {
     }).then();
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
