38b3afbd2c437510709af76726e898edc0b9a481
REIRIKU <cookiecat3003@gmail.com>
2025-10-15 15:16:52 +0900

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,6 @@ const execPromise = promisify(exec);
 const axios = require('axios');
 const util = require('util');
 const msgpack = require('@msgpack/msgpack');
-const bcrypt = require('bcryptjs');
     
 global.debug = process.env.NODE_ENV === 'development';
 global.__THETREE__ = {};
